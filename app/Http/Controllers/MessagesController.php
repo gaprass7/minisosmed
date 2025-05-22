1921682d6ed5da32f20277753c1340aff2eb8da0
gaprass7 <gnngzr@gmail.com>
2025-05-22 17:35:49 +0700

    membuat api messages (insert, detail & delete)

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Message;
+
+class MessagesController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'sender_id' => 'required|integer',
+            'receiver_id' => 'required|integer',
+            'message_content' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        //jika validasi berhasil
+        $message = Message::create([
+            'sender_id' => $request->sender_id,
+            'receiver_id' => $request->receiver_id,
+            'message_content' => $request->message_content
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil Mengirim Pesan Baru',
+            'data' => $message
+        ], 201);
+    }
+
+    public function show($id) {
+
+        $message = Message::find($id);
+        return response()->json([
+            'success' => true,
+            'data' => $message
+        ]);
+
+    }
+
+
+    public function destroy($id)
+    {
+        Message::destroy($id);
+        
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil Menghapus Pesan'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CommentsController;
 use App\Http\Controllers\LikesController;
+use App\Http\Controllers\MessagesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,5 +26,10 @@ Route::prefix('v1')->group(function () {
     Route::prefix('likes')->group(function () {
         Route::post('/', [likesController::class, 'store']);
         Route::delete('{id}', [likesController::class, 'destroy']);
+    });
+    Route::prefix('messages')->group(function () {
+        Route::post('/', [MessagesController::class, 'store']);
+        Route::get('{id}', [MessagesController::class, 'show']);
+        Route::delete('{id}', [MessagesController::class, 'destroy']);
     });
 });
